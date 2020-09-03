336978300729f83d3bb13da078f073ca93c4b4c6
shaoqing <xuesq@pvc123.com>
2020-09-03 15:12:02 +0800

    Initial Commit

---
+++ Xib2Code/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xib2Code
+//
+//  Created by iMac on 2020/9/3.
+//  Copyright © 2020 iMac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
